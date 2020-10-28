59366d3a6bcb9a119d49587d99d9d0d606283a75
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-10-28 20:01:25 +0000

    typo fixed

--- spec/features/feature_test_for_bday_person_spec.rb
+++ spec/features/feature_test_for_bday_person_spec.rb
@@ -15,13 +15,13 @@ feature 'Testing the results' do
     fill_in('name', with: 'John')
     fill_in "day", with: "#{(DateTime.now + 1).strftime("%Y-%m-%d")}"
     click_button('Go!')
-    expect(page).to  have_content('Your birthday will be be in 1 day, John.')
+    expect(page).to  have_content('Your birthday will be in 1 day, John.')
   end
   scenario 'can inform user of how many days there is until their birthday' do
     visit('/')
     fill_in('name', with: 'John')
     fill_in "day", with: "#{(DateTime.now + 2).strftime("%Y-%m-%d")}"
     click_button('Go!')
-    expect(page).to  have_content('Your birthday will be be in 2 days, John.')
+    expect(page).to  have_content('Your birthday will be in 2 days, John.')
   end
 end
